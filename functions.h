1ac76f4da9db61ecc1730a770aafb7aca723484a
Taylor <tay333@gmail.com>
2012-11-25 23:28:52 -0800

    request and ack

---
+++ functions.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+
+#define DATAGRAM_SIZE 1000
+#define HEADER_SIZE  2 * sizeof(int)
+#define DATA_SIZE (DATAGRAM_SIZE - HEADER_SIZE)
+
+void error(char *msg) 
+{
+    perror(msg);
+    exit(1);
+}
+
+struct packet
+{
+    int ack;
+    int seq;
+    char data[DATA_SIZE]; 
+};
