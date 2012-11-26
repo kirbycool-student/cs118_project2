a5c4c688aa46b0fbd5c0b11b0d6e2a4b467d0dfa
Taylor <tay333@gmail.com>
2012-11-26 15:41:52 -0800

    receiver cleanup

--- receiver.c
+++ receiver.c
@@ -11,10 +11,13 @@
 #include "functions.h"
 
 int main(int argc, char *argv[]) {
+
+//******************initializations**********************************//
     int sock, port, nbytes;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
+    //usage
     if (argc < 4) {
        fprintf(stderr,"usage %s hostname port message\n", argv[0]);
        exit(0);
@@ -34,9 +37,9 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_family = AF_INET; //initialize server's address
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(port);
-
     printf("port: %d \n", ntohs(serv_addr.sin_port));
     
+//******************handshake**********************************//
     struct packet initPacket;
     initPacket.ack = 0;
     initPacket.seq = 1;
@@ -54,19 +57,20 @@ int main(int argc, char *argv[]) {
     //print diagnostic to console
     char addr[256];
     inet_ntop(AF_INET, &(serv_addr.sin_addr), addr, INET_ADDRSTRLEN); 
-
     printf ("Receiver: Sent message: %s To: %s : %d \n", initPacket.data, addr, serv_addr.sin_port);
+
+//******************main loop for receiving data*******************//
+
+    fprintf (stderr, "waiting for message \n");
+    struct packet incoming;
+    struct packet outgoing;
+    outgoing.ack = 1;
+    bzero(incoming.data,DATA_SIZE);
+    bzero(outgoing.data,DATA_SIZE);
 
     while(1) {
 
         //wait for a packet
-        fprintf (stderr, "waiting for message \n");
-        struct packet incoming;
-        struct packet outgoing;
-        outgoing.ack = 1;
-        bzero(incoming.data,DATA_SIZE);
-        bzero(outgoing.data,DATA_SIZE);
-
         size = sizeof(serv_addr);
         if( nbytes = recvfrom(sock, &incoming, DATAGRAM_SIZE, 0, 
                                 (struct sockaddr *) &serv_addr,
@@ -80,25 +84,28 @@ int main(int argc, char *argv[]) {
         inet_ntop(AF_INET, &(serv_addr.sin_addr), addr, INET_ADDRSTRLEN);
 
         if (incoming.ack == 1) 
+        //first pkt is ack of request
         {
             fprintf (stderr, "Receiver: got ack for: %d From: %s : %d\n", incoming.seq, addr, serv_addr.sin_port);
         }
         else  
+        // data packet
         {
             fprintf (stderr, "Receiver: got test message for: %d From: %s : %d\n", incoming.seq, addr, serv_addr.sin_port);
+
+            //send corresponding ack 
             outgoing.seq = incoming.seq;
-            //send the message
             int size = sizeof(serv_addr);
             if( nbytes = sendto (sock, &outgoing, DATAGRAM_SIZE, 0,
                                 (struct sockaddr *) &serv_addr , size) < 0)
             {
                 error("sendto failed");
             }    
+
             //print diagnostic to console
             char addr[256];
             inet_ntop(AF_INET, &(serv_addr.sin_addr), addr, INET_ADDRSTRLEN); 
             printf ("Receiver: Sent ack for: %d To: %s : %d \n", outgoing.seq, addr, serv_addr.sin_port);
-
         }
     }   
 }
