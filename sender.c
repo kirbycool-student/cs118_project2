e37c567362764ab76fc355336bb2b8657d884870
Taylor <tay333@gmail.com>
2012-12-02 15:56:46 -0800

    comments and receiver winzize=1

--- sender.c
+++ sender.c
@@ -10,6 +10,8 @@
 #include "functions.h"
 
 int main(int argc, char *argv[]) {
+    
+    ///////////////*****SETUP****/////////////////
     int sock, port, nbytes, size, base;
     struct sockaddr_in serv_addr, client_addr;
 
@@ -40,6 +42,7 @@ int main(int argc, char *argv[]) {
     int acks[windowSize];
     char fileName[DATA_SIZE];
 
+    ///////////////*****HANDSHAKE****/////////////////
     //wait for connections
     struct packet handshake;
 
@@ -88,7 +91,7 @@ int main(int argc, char *argv[]) {
         error("failed open file"); 
     }
 
-    //initialize and send the first window of packets
+    ////////////initial window of packets////////////
     base = 1;
     int k;
     for(k = 0; k < windowSize; k++) {
